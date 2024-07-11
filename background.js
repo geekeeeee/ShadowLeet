b8ba07154b4570d8d88a846ee10c7efed505e978
abijith r <93731985+geekeeeee@users.noreply.github.com>
2024-07-11 19:21:03 +0530

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.todo == "showPageAction"){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             if(tabs && tabs.length()>0)chrome.pageAction.show(tabs[0].id);
-            else console.error("no tabs foud")
+            else console.error("no tabs found")
         })
     }
 })
